28a5afa981cc3fe2b8c8f8f2a23ad1dacb0c609d
Liya Susan Biju <liyasusanbiju@gmail.com>
2023-06-10 14:34:18 +0530

    some fixes

--- app/_components/About.tsx
+++ app/_components/About.tsx
@@ -18,7 +18,7 @@ function About() {
         Three bootcamps. <br/>
         And one hackathon. </p>
        </p>
-       <div className='about-content-inner'><b>GDSC MBCET</b> presents <b>TechnoVerse</b> where Web, ML and Blockchain all come together to make the ultimate collaborative experience. Spanning over 5 days & culminating in a 2 day hackathon, participants in each track can come together and <b>create cool stuff that matters</b>! </div>
+       <div className='about-content-inner'><span className='bold'>GDSC MBCET</span> presents<span className='bold'> TechnoVerse</span> where Web, ML and Blockchain all come together to make the ultimate collaborative experience. Spanning over 5 days & culminating in a 2 day hackathon, participants in each track can come together and <span className='bold'>create cool stuff that matters</span>! </div>
        </div>
        <p className='promo-vid'>promo here</p>
        </div>
